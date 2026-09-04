d3d1912f0979ff24da2e85981722df3dab52bfb3
husnieez <husnimuszdalifah.kuliah@gmail.com>
2026-09-04 09:57:51 +0700

    [P-2] setup routing resource semua tabel

---
+++ app/Http/Controllers/LoanController.php
@@ -0,0 +1,43 @@
+<?php
+// File: app/Http/Controllers/LoanController.php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LoanController extends Controller
+{
+    public function index()
+    {
+        return 'LoanController@index';
+    }
+
+    public function create()
+    {
+        return 'LoanController@create';
+    }
+
+    public function store(Request $request)
+    {
+        return 'LoanController@store';
+    }
+
+    public function show(string $id)
+    {
+        return "LoanController@show, id: {$id}";
+    }
+
+    public function edit(string $id)
+    {
+        return "LoanController@edit, id: {$id}";
+    }
+
+    public function update(Request $request, string $id)
+    {
+        return "LoanController@update, id: {$id}";
+    }
+
+    public function destroy(string $id)
+    {
+        return "LoanController@destroy, id: {$id}";
+    }
+}
